3cc39d972115ab70a0991d57aa0df589276f60a2
nitin <nitinkoolwal999@gmail.com>
2023-04-16 14:47:08 +0530

    users validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length:{minimun:3, maximum:25}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness:true, length:{maximum:105},format: {with: VALID_EMAIL_REGEX}
+
 end
