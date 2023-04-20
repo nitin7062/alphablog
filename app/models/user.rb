df0de8573ce0ce409ace1dd7e018768c81b1ab58
nitin <nitinkoolwal999@gmail.com>
2023-04-20 17:43:14 +0530

    bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   validates :username, presence: true, length:{minimun:3, maximum:25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness:true, length:{maximum:105},format: {with: VALID_EMAIL_REGEX}
-  has_many :articles
+  has_many :articless
 
+  def downcase_email
+    self.email = email.downcase
+  end
 end
